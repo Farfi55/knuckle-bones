8e8c341c700f2b3c65e0f778b8d2c96c00c122ec
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-11-09 02:08:31 +0100

    Agent is now a MonoBehaviour Abstract class
    some logic moved from Player into Agent

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -3,8 +3,23 @@ using System.Collections.Generic;
 using System.Security.Authentication.ExtendedProtection;
 using UnityEngine;
 
-public interface IAgent
+public abstract class Agent : MonoBehaviour
 {
-    int RollDice();
-    void PlaceDie(int dieValue, FullBoard fullBoard, int side);
+    protected int side = -1;
+    protected Board board;
+    public abstract int RollDice();
+    // void ChoseColumnToPlaceDie(int dieValue, FullBoard fullBoard);
+    
+    public void SetSide(int side) => this.side = side;
+    public void SetSide(Board board) => this.board = board;
+    
+    
+    
+    protected bool IsMyTurn()
+    {
+        return side == GameManager.Instance.CurrentPlayerSide;
+    }
+    
+    
+}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,42 +2,59 @@ using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Player : MonoBehaviour, IAgent
+public class Player : Agent
 {
-    public int RollDice()
+    public override int RollDice()
     {
         var dice = Random.Range(1, 7);
         Debug.Log(dice);
         return dice;
     }
 
-    public void PlaceDie(int dieValue, FullBoard fullBoard, int side)
-    {
-        
-    }
+    public int rolledDie = 0;
 
 
     private void Update()
     {
-        if(!Input.anyKeyDown)
+        if (!IsMyTurn())
             return;
+
+        if (!Input.anyKeyDown)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rolledDie = RollDice();
+        }
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-                
+            PlaceDie(0);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            
+            PlaceDie(1);            
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            
+            PlaceDie(2);
         }
-        else
+    }
+
+    private void PlaceDie(int col)
+    {
+        if (board.IsColFull(col))
         {
-            
+            Debug.LogWarning("Column is full");
+            return;
         }
-        
+        if (rolledDie == 0)
+        {
+            Debug.LogWarning("You have not rolled the die");
+            return;
+        }
+
+        board.PlaceDie(rolledDie, col);
+        rolledDie = 0;
     }
 }
